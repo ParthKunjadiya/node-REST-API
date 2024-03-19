0df8845d4becd2fceeb8e480cc4c8dde09c67ce9
ParthKunjadiya <parthkunjadiya3@gmail.com>
2024-03-19 10:19:32 +0530

    complete REST API (use get, post, put, delete)

--- app.js
+++ app.js
@@ -3,13 +3,39 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const multer = require('multer');
+const cors = require('cors');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
+app.use(cors());
 
-// app.use(bodyParser.urlencoded()); // use this for => x-www-urlencoded <form> data
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
+app.use(bodyParser.urlencoded({ extended: true })); // use this for => x-www-urlencoded <form> data
 app.use(bodyParser.json()); // use this for => application/json name in header
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
@@ -20,12 +46,14 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode;
     const message = error.message;
-    res.status(status).json({ message: message });
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
 });
 
 mongoose.connect('mongodb+srv://Parth:%40Parth45%40@cluster0.tajycs5.mongodb.net/Messages?retryWrites=true&w=majority&appName=Cluster0')
--- routes/feed.js
+++ routes/feed.js
@@ -2,15 +2,17 @@ const express = require('express');
 const { body } = require('express-validator');
 
 const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 // /feed/posts => GET
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
 // /feed/post => POST
 router.post(
     '/post',
+    isAuth,
     [
         body('title')
             .trim()
@@ -23,6 +25,22 @@ router.post(
 );
 
 // /feed/posts => GET
-router.get('/post/:postId', feedController.getPost);
+router.get('/post/:postId', isAuth, feedController.getPost);
+
+router.put(
+    '/post/:postId',
+    isAuth,
+    [
+        body('title')
+            .trim()
+            .isLength({ min: 5 }),
+        body('content')
+            .trim()
+            .isLength({ min: 5 })
+    ],
+    feedController.updatePost
+);
+
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;
